0fcf9359e4be448dff87e627d2e112f893e67fa1
Nam-Wijeong <dnlwjdv@hanmail.net>
2022-12-09 11:32:05 +0900

    대문자변환 함수 추가

--- src/components/pokemonInfo/PokemonInfo.tsx
+++ src/components/pokemonInfo/PokemonInfo.tsx
@@ -3,22 +3,27 @@ import styled from 'styled-components';
 
 const PokemonInfo = ({ pokemonData }: null | any) => {
   const imgSrc = pokemonData.data.sprites.other.dream_world.front_default;
-  const defaultImgSrc = pokemonData.data.sprites.front_default
+  const defaultImgSrc = pokemonData.data.sprites.front_default; // imgSrc의 값이 null일 때
   const name = pokemonData.data.name;
   const id = pokemonData.data.id;
   const weight = pokemonData.data.weight;
-  const ability = `${pokemonData.data.abilities[0].ability.name}`
+  const ability = `${pokemonData.data.abilities[0].ability.name}`;
+
+  // 첫번째 글자를 대문자로 변환하기
+  const changeToCapLetter = (letter: string) => {
+    return letter[0].toUpperCase() + letter.slice(1);
+  }
 
   return (
     <PokemonWrap>
       <PokemonImgWrap>
         <PokemonImg src={!imgSrc ? defaultImgSrc : imgSrc} alt={`${name}_image`} />
-        <PokemonName>{name}</PokemonName>
+        <PokemonName>{changeToCapLetter(name)}</PokemonName>
       </PokemonImgWrap>
       <PokemonInfoList>
         <PokemonInfoItem>ID : #{id}</PokemonInfoItem>
         <PokemonInfoItem>WEIGHT : {weight}</PokemonInfoItem>
-        <PokemonInfoItem>ABILITY : {ability}</PokemonInfoItem>
+        <PokemonInfoItem>ABILITY : {changeToCapLetter(ability)}</PokemonInfoItem>
       </PokemonInfoList>
     </PokemonWrap >
   )
